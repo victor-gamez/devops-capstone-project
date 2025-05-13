08f314ce3dfa44633eb89eeaa29f7aef4dead8da
victor-gamez <victor.gamez@qaracter.es>
2025-05-13 06:19:06 -0400

    Delete  accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -157,4 +157,10 @@ class TestAccountService(TestCase):
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")   
+        self.assertEqual(updated_account["name"], "Something Known")
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
